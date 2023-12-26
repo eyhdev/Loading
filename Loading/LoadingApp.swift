51230497f1f26e29782a4845029513c0058ba157
eyhdev <alaqsa335@gmail.com>
2023-12-26 14:24:23 +0300

    Initial Commit

---
+++ Loading/LoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingApp.swift
+//  Loading
+//
+//  Created by eyh.mac on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
