0a9437d4956f4f07d2f7ac4ed691daed2dd36686
eyhdev <alaqsa335@gmail.com>
2023-12-26 14:37:04 +0300

    Loading

--- Loading/ContentView.swift
+++ Loading/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+ @State private var isRotating = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Circle()
+            .trim(from: 0, to: 0.7)
+            .stroke(AngularGradient(gradient: Gradient(colors: [.white, .white]), center: .center), lineWidth: 5)
+            .frame(width: 50, height: 50)
+            .rotationEffect(Angle(degrees: isRotating ? 360 : 0))
+            .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false), value: isRotating)
+            .onAppear() {
+                self.isRotating = true
+            }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
